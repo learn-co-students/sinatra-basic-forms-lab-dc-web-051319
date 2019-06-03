757dd3c3153bf368f109fe1abcb41ee162a517b0
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-06-03 15:19:07 -0400

    Completed lab

---
+++ models/puppy.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Puppy
+  attr_reader :breed
+  attr_accessor :months_old, :name
+
+  def initialize(name, breed, months_old)
+    @name = name
+    @breed = breed
+    @months_old = months_old
+  end
+end
